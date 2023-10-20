12f2c83bea13927320c580d5a3f73ef67cff91c9
Shokhrukhbek Yuldoshev <shokhrukhbekdev@gmail.com>
2023-10-21 00:14:47 +0300

    Added token.

--- src/app/_libs/api.ts
+++ src/app/_libs/api.ts
@@ -1,7 +1,13 @@
 // search top 10 users by location
 export const searchTopUsers = async (location: string) => {
+    // give header to avoid rate limit
     const res = await fetch(
-        `https://api.github.com/search/users?q=location:${location}&sort=followers&order=desc&per_page=30`
+        `https://api.github.com/search/users?q=location:${location}&sort=followers&order=desc&per_page=30`,
+        {
+            headers: {
+                Authorization: `token ${process.env.GITHUB_TOKEN}`,
+            },
+        }
     );
     const data = await res.json();
     return data.items;
@@ -9,7 +15,13 @@ export const searchTopUsers = async (location: string) => {
 
 // fetch user details by username
 export const getUserDetails = async (username: string) => {
-    const res = await fetch(`https://api.github.com/users/${username}`);
+    // give header to avoid rate limit
+    const res = await fetch(`https://api.github.com/users/${username}`, {
+        headers: {
+            Authorization: `token ${process.env.GITHUB_TOKEN}`,
+        },
+    });
+
     const data = await res.json();
     return data;
 };
